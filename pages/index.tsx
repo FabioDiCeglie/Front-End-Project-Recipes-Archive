489f27dc45959915ea15cde8d245ce1d108a799d
FabioDiCeglie <dcfabio96@gmail.com>
2022-06-23 08:26:07 +0200

    cleaning some code

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,10 +1,12 @@
 import { useQuery, gql } from "@apollo/client";
-import Card from "@mui/material/Card";
-import CardActions from "@mui/material/CardActions";
-import CardContent from "@mui/material/CardContent";
-import CardMedia from "@mui/material/CardMedia";
-import Button from "@mui/material/Button";
-import Typography from "@mui/material/Typography";
+import {
+  Card,
+  CardActions,
+  CardContent,
+  CardMedia,
+  Button,
+  Typography,
+} from "@mui/material";
 
 interface Recipe {
   name: String;
@@ -28,17 +30,15 @@ export default function Home() {
   console.log(data.getRecipes);
   return (
     <div>
-      <h1>{data.getRecipes.map((recipe: Recipe) => recipe.name)}</h1>
       <Button>Hello</Button>
       <Card sx={{ maxWidth: 345 }}>
         <CardMedia component="img" height="140" image="" alt="green iguana" />
         <CardContent>
           <Typography gutterBottom variant="h5" component="div">
-            Lizard
+            {data.getRecipes.map((recipe: Recipe) => recipe.name)}
           </Typography>
           <Typography variant="body2" color="text.secondary">
-            Lizards are a widespread group of squamate reptiles, with over 6,000
-            species, ranging across all continents except Antarctica
+            {data.getRecipes.map((recipe: Recipe) => recipe.description)}
           </Typography>
         </CardContent>
         <CardActions>
