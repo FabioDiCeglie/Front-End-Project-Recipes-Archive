5314d4f8b4c5d6a1232319099130bd334dec6246
FabioDiCeglie <dcfabio96@gmail.com>
2022-06-23 08:24:18 +0200

    Introducing Card for Recipes

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,6 +1,17 @@
 import { useQuery, gql } from "@apollo/client";
+import Card from "@mui/material/Card";
+import CardActions from "@mui/material/CardActions";
+import CardContent from "@mui/material/CardContent";
+import CardMedia from "@mui/material/CardMedia";
+import Button from "@mui/material/Button";
+import Typography from "@mui/material/Typography";
 
-const QUERY: any = gql`
+interface Recipe {
+  name: String;
+  description: String;
+}
+
+const QUERY = gql`
   query GetRecipes {
     getRecipes {
       name
@@ -17,7 +28,24 @@ export default function Home() {
   console.log(data.getRecipes);
   return (
     <div>
-      <h1>{data.getRecipes.map((recipe: any) => recipe.name)}</h1>
+      <h1>{data.getRecipes.map((recipe: Recipe) => recipe.name)}</h1>
+      <Button>Hello</Button>
+      <Card sx={{ maxWidth: 345 }}>
+        <CardMedia component="img" height="140" image="" alt="green iguana" />
+        <CardContent>
+          <Typography gutterBottom variant="h5" component="div">
+            Lizard
+          </Typography>
+          <Typography variant="body2" color="text.secondary">
+            Lizards are a widespread group of squamate reptiles, with over 6,000
+            species, ranging across all continents except Antarctica
+          </Typography>
+        </CardContent>
+        <CardActions>
+          <Button size="small">Share</Button>
+          <Button size="small">Learn More</Button>
+        </CardActions>
+      </Card>
     </div>
   );
 }
