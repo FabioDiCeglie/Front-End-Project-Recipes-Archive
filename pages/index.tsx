ab0e063eda3aaa0a671d2c22dea60cd08622a4d5
FabioDiCeglie <dcfabio96@gmail.com>
2022-06-27 08:11:39 +0200

    Adjusting Homepage component

--- pages/index.tsx
+++ pages/index.tsx
@@ -28,23 +28,25 @@ export default function Home() {
     <Container sx={{ mt: 12 }}>
       {data.getRecipes.map((recipe: Recipe, i: number) => {
         return (
-          <Card sx={{ maxWidth: 300 }} key={i}>
-            <CardMedia
-              component="img"
-              height="140"
-              // @ts-ignore
-              image={recipe.imageUrl}
-              alt={recipe.name}
-            />
-            <CardContent>
-              <Typography gutterBottom variant="h5" component="div">
-                {recipe.name}
-              </Typography>
-              <Typography variant="body2" color="text.secondary">
-                {recipe.description}
-              </Typography>
-            </CardContent>
-          </Card>
+          <Container sx={{ mt: 6 }}>
+            <Card sx={{ maxWidth: 300 }} key={i}>
+              <CardMedia
+                component="img"
+                height="140"
+                // @ts-ignore
+                image={recipe.imageUrl}
+                alt={recipe.name}
+              />
+              <CardContent>
+                <Typography gutterBottom variant="h5" component="div">
+                  {recipe.name}
+                </Typography>
+                <Typography variant="body2" color="text.secondary">
+                  {recipe.description}
+                </Typography>
+              </CardContent>
+            </Card>
+          </Container>
         );
       })}
     </Container>
